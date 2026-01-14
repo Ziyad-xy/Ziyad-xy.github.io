bfa6db0c02a7dbe376caff0c6e26032652daa652
Ziyad-xy <ziyad.ahmed2500272@compit.aun.edu.eg>
2026-01-14 14:15:17 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+function makeBlue() {
+    document.body.style.backgroundColor = "#00a8ff";
+    document.body.style.color = "#130f40";
+}
+
+function makeGreen() {
+    document.body.style.backgroundColor = "#178106";
+    document.body.style.color = "#000000";
+}
+
+function makePink() {
+    document.body.style.backgroundColor = "#e84393";
+    document.body.style.color = "#b33939";
+}
